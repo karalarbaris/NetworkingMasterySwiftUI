07a061f4951f8f5b72eea26f2ab1522d8c090fb8
Baris Karalar <karalarbaris@gmail.com>
2024-01-04 14:02:21 +0100

    Initial Commit

---
+++ NetworkingMasterySwiftUI/NetworkingMasterySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingMasterySwiftUIApp.swift
+//  NetworkingMasterySwiftUI
+//
+//  Created by Baris Karalar on 04.01.24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingMasterySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
